3f7462577304b0c81f0d1620ecc14a94e4a15a75
André Victor <andrevictor50@gmail.com>
2020-04-21 15:10:24 -0300

    finalização do desafio

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -23,19 +25,37 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    async function processArray(): Promise<void> {
+      for (const uploadedFile of uploadedFiles) {
+        data.append('file', uploadedFile.file);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
+        try {
+          await api.post('/transactions/import', data);
+        } catch (err) {
+          console.log(err.response.error);
+        }
+
+        data.delete('file');
+      }
     }
+
+    await processArray();
+
+    history.push('/');
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    files.forEach((file) => {
+      const newFile: FileProps = {
+        file,
+        name: file.name,
+        readableSize: String(file.size),
+      };
+
+      setUploadedFiles([...uploadedFiles, newFile]);
+    });
   }
 
   return (
